33ef3ca4ceea99bd4d0fa708932e3fa578b08f49
Coindroid <mail@coindroid.org>
2019-10-18 22:36:44 +0300

    update

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -287,11 +287,11 @@ public:
         nDefaultPort = 7811;
         nPruneAfterHeight = 100000;
 
-<<<<<<< HEAD
+
         genesis = CreateGenesisBlock(1571426618, 46509, 0x1e0ffff0, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
 
-        consensus.hashGenesisBlock = genesis.GetHash();
+
         // calculate main genesis block
         //consensus.hashGenesisBlock = uint256S("0x00");
         if (true && (genesis.GetHash() != consensus.hashGenesisBlock)) {
@@ -316,64 +316,6 @@ public:
 
         assert(consensus.hashGenesisBlock == uint256S("0x000003bb3d784075f6f77997fa8f4abc455ac23e055a0f13b22d643d7d7f13c1"));
         assert(genesis.hashMerkleRoot == uint256S("0x61be29d3a529ae26ac6b760468e6ed613a1e86636fb8e4266437d39d64a0cda4"));
-
-=======
-		  genesis = CreateGenesisBlock(15713019664, 46509, 0x1e0ffff0, 1, 50 * COIN);
-        consensus.hashGenesisBlock = genesis.GetHash();
-//        assert(consensus.hashGenesisBlock == uint256S("0x000003bb3d784075f6f77997fa8f4abc455ac23e055a0f13b22d643d7d7f13c1"));
-//        assert(genesis.hashMerkleRoot == uint256S("0x61be29d3a529ae26ac6b760468e6ed613a1e86636fb8e4266437d39d64a0cda4"));
-
-
- /*       printf("Searching for genesis block...\n");
-         uint256S hashTarget = uint256S().SetCompact(genesis.nBits);
-         uint256S thash;
-
-         while (true)
-         {
-            thash = genesis.GetHash();
-            if (thash <= hashTarget)
-                break;
-            if ((genesis.nNonce & 0xFFF) == 0)
-            {
-                printf("nonce %08X: hash = %s (target = %s)\n", genesis.nNonce, thash.ToString().c_str(), hashTarget.ToString().c_str());
-            }
-            ++genesis.nNonce;
-            if (genesis.nNonce == 0)
-            {
-                printf("NONCE WRAPPED, incrementing time\n");
-                ++genesis.nTime;
-            }
-        }
-        printf("genesis.nTime = %u \n", genesis.nTime);
-        printf("genesis.nNonce = %u \n", genesis.nNonce);
-        printf("genesis.nVersion = %u \n", genesis.nVersion);
-        printf("genesis.nBits = %u \n", genesis.nBits);
-        printf("genesis.GetHash = %s\n", genesis.GetHash().ToString().c_str()); 
-        printf("genesis.hashMerkleRoot = %s \n", genesis.hashMerkleRoot.ToString().c_str()); 
-*/
-  //if (true && (genesis.GetHash() != consensus.hashGenesisBlock)) {
-  		std::cout << std::string("Calculating main genesis block...\n");
-              arith_uint256 hashTarget = arith_uint256().SetCompact(genesis.nBits);
-              uint256 hash;
-              genesis.nNonce = 0;
-              while (UintToArith256(genesis.GetHash()) > hashTarget)
-              {
-                 printf("nonce %08X: hash = %s (target = %s)\n", genesis.nNonce, hash.ToString().c_str(), hashTarget.ToString().c_str());
-                  ++genesis.nNonce;
-                  if (genesis.nNonce == 0)
-                  {
-                      ++genesis.nTime;
-							 
-                  }
-              }
-              std::cout << "Genesis block found!\n";
-              std::cout << "nonce: " << genesis.nNonce << "\n";
-              std::cout << "time: " << genesis.nTime << "\n";
-              std::cout << "blockhash: " << genesis.GetHash().ToString().c_str() << "\n";
-              std::cout << "merklehash: " << genesis.hashMerkleRoot.ToString().c_str() << "\n";
- //         }
->>>>>>> b74981e3adf497551a26d48cfe3039f7526b25bd
-
 
         vSeeds.push_back(CDNSSeedData("dev.cannabitz.io", "dev.cannabitz.io"));
         vSeeds.push_back(CDNSSeedData("node1.cannabitz.io", "node1.cannabitz.io"));
